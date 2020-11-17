3adf857274a46fd8b946ea5f305ddd112a9fa82a
Sky Include <73770029+skyinclude@users.noreply.github.com>
2020-11-18 07:07:21 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Factory!</a>
+          Welcome to <a href="https://nextjs.org">我的工厂</a>
         </h1>
 
         <p className={styles.description}>
